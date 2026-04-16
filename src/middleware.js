71ef1cb7b8f3e560f23c6189cf9f88502a02fb9d
webitya <webitya@gmail.com>
2026-04-16 19:33:53 +0530

    Aditya

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,10 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/admin/login",
+  },
+});
 
 export const config = { 
   matcher: ["/admin/dashboard/:path*", "/admin/products/:path*", "/admin/leads/:path*"] 
